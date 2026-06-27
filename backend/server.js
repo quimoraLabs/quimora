c9ecb185055da5ce594269ea970f0f115189985f
madhav kumar <madhavxman@gmail.com>
2026-06-28 03:11:25 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,10 @@ import config from "./config/config.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import quizRoutes from "./routes/quiz.routes.js";
-import attemptQuizRoutes from "./routes/attempQuiz.routes.js"
+import attemptQuizRoutes from "./routes/attempQuiz.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import questionRoutes from "./routes/question.routes.js";
 import { validateObjectId } from "./middleware/validObjectId.middleware.js";
-
 
 const app = express();
 if (config.nodeENV !== "production") {
@@ -18,10 +17,11 @@ if (config.nodeENV !== "production") {
 }
 
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' 
-    ? 'https://your-live-quimora-app.com' 
-    : 'http://localhost:5173',
-  optionsSuccessStatus: 200 
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://quimorabackend.vercel.app"
+      : "http://localhost:5173",
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/quizzes", quizRoutes);
-app.use("/api/attempts",attemptQuizRoutes);
+app.use("/api/attempts", attemptQuizRoutes);
 app.use(
   "/api/quiz/:quizId/questions",
   validateObjectId("quizId"),
@@ -39,7 +39,11 @@ app.use(
 // Global error handler
 app.use(errorHandler);
 
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
-  connectDB();
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+    connectDB();
+  });
+}
+
+export default app;
